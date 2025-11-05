f2a86141786b231eb8a24959d61400fdc242d29e
Keshav Maheshwari <Keshav1534@gmail.com>
2025-11-05 16:16:43 +0530

    Added .net mvc. Routed the static files.

---
+++ BharatiRadio/BharatiRadio/Controllers/ShowsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BharatiRadio.Controllers
+{
+    public class ShowsController : Controller
+    {
+        // GET: Shows
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
