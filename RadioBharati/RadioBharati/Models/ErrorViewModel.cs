a2d4224a736eecd4a888ded5cfaebc4f86c0f375
kritika-kj <kritikakalonia@gmail.com>
2025-11-30 17:40:59 +0530

    Removed unneccassary files

--- RadioBharati/RadioBharati/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RadioBharati.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
